9e020389d5d1715b4df73d7669c508636c46e23d
harpreet-singh-au7 <simransingh12340@gmail.com>
2020-10-30 01:52:13 +0530

    App deployed

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDjW8N0XNy--p9TQq2Moc1f-_dagCtLkR4",
+  authDomain: "todocrud-4f528.firebaseapp.com",
+  databaseURL: "https://todocrud-4f528.firebaseio.com",
+  projectId: "todocrud-4f528",
+  storageBucket: "todocrud-4f528.appspot.com",
+  messagingSenderId: "54286326806",
+  appId: "1:54286326806:web:66e0c2cdd01610803b2937",
+};
